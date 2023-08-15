81605caeef0f152599e01b953b8d02ffda24b343
jon.randyl <jon.randyl@wela.online>
2023-08-15 11:33:55 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+
+func main() {
+
+ godotenv.Load(".env")
+
+ portString := os.Getenv("PORT")
+
+ if portString == "" {
+	log.Fatal("PORT is not found in the environment")
+ }
+
+ router := chi.NewRouter()
+
+ router.Use(cors.Handler(cors.Options{
+	AllowedOrigins:   []string{"https://*", "http://*"},
+	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders:   []string{"*"},
+	ExposedHeaders:   []string{"Link"},
+	AllowCredentials: false,
+	MaxAge:           300,
+}))
+
+ serve := &http.Server{
+	Handler: router,
+	Addr: ":" + portString,
+ }
+
+ log.Printf("Server starting on port %v", portString)
+ err := serve.ListenAndServe()
+ if err != nil {
+	log.Fatal(err)
+ }
+}
